bfdb01e4e606749fefbaffbba0d187d8f08a06f2
ExcaliburKG <akagorbunov@ya.ru>
2023-06-28 11:22:02 +0300

    initial

---
+++ step.dev.py
@@ -0,0 +1,13 @@
+from sinara.step import Step
+from sinara.step import StepSafeguard as sg
+
+try:
+    step = Step(step_params_file_globs="params/step_params.json",
+                pipeline_params_file_globs="params/pipeline_params.json",
+           env_name="user")
+    for notebook in step.notebooks:
+        notebook.run()
+except Exception as e:
+    step.handle_exception(e)
+finally:
+    step.handle_exit()
