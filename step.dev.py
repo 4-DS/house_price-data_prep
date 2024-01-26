c9d6a6303e60279ee35eb482853962cf15c12289
pestovid <128602207+pestovid@users.noreply.github.com>
2024-01-26 08:00:28 +0300

    Update step.dev.py

--- step.dev.py
+++ step.dev.py
@@ -1,5 +1,6 @@
 from sinara.step import Step
 from sinara.step import StepSafeguard as sg
+from sinara.step import StepReport as sr
 
 try:
     step = Step(step_params_file_globs="params/step_params.json",
@@ -9,4 +10,5 @@ try:
 except Exception as e:
     step.handle_exception(e)
 finally:
+    sr.save()
     step.handle_exit()
